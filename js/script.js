0d657ae0b3cae260c83e3c7759116054e0520195
Clayton Rieck <cjrieck123@gmail.com>
2014-02-12 21:40:50 -0500

    Nav bar working. Need to add hrefs and continue with content

--- js/script.js
+++ js/script.js
@@ -1,25 +1,61 @@
 $(function () {
+	
+	window.scrollTo(0,0);
 
-	$(window).scroll(function(){
+	var atTop = !$(document).scrollTop();
 
-		var offset = $('.container').offset().top;
-		console.log(offset);
+	var offset = $('.container').offset().top;
+	var projectsTop = $('#projects').offset().top;
 
-		if ($(window).scrollTop() >= offset)
+	$(window).scroll(function() {
+
+		
+		var currPos = $(window).scrollTop();
+
+		if (currPos < offset && !atTop)
 		{
-			console.log($(window).scrollTop());
-			// show nav bar in here
+
+			$('#navbar').animate({top: '-100%'}, 500);
+			atTop = true;
+
+		} else if (currPos >= offset && atTop) {
+
 			$('#navbar').animate({top: '0%'}, 500);
+			atTop = false;
 		}
 
-		// if ($(window).scrollTop() <= offset){
-		// 	console.log($(window).scrollTop());
-		// 	// show nav bar in here
-		// 	$('#navbar').animate({top: '-100%'}, 500);
-		
-		// }
+		console.log(projectsTop);
 
-		return false;
+		if (currPos > projectsTop) {
+			$('#navbar ul').stop().animate({
+					backgroundColor: '#ffffff'
+				}, 100);
+
+			$('#navbar ul li a').stop().animate({
+					color: '#000000'
+				}, 100);
+
+			$('#twitter').attr('src', 'img/twitter_black.png');
+			$('#linkedin').attr('src', 'img/linkedin_black.png');
+			$('#github').attr('src', 'img/octocat_black.png');	
+		
+		} else {
+		
+			$('#navbar ul').stop().animate({
+					backgroundColor: '#000000',
+					color: '#ffffff'
+				}, 100);
+
+			$('#navbar ul li a').stop().animate({
+					color: '#ffffff'
+				}, 100);
+
+			$('#twitter').attr('src', 'img/twitter_white.png');
+			$('#linkedin').attr('src', 'img/linkedin_white.png');
+			$('#github').attr('src', 'img/octocat_white.png');
+			
+		}
+
 	});
 	
 });
