e4365349dd4a10c59f002a1bee21755bb2dfb5e9
Rieck <rieckc@NY-Rieckc-MV.local>
2015-03-06 14:28:20 -0500

    moved footer to container

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function(){
-	// Calculate height of page here
-
 	$('html, body').stop().animate({
 		scrollTop: 0
 	}, 1000);
@@ -39,7 +37,6 @@ $(function() {
 			{
 
 				$('#navbar').removeClass("active");
-				// $('#navbar').stop().animate({top: '-100%'}, 800);
 				atTop = true;
 
 				// name on blurred picture
@@ -47,49 +44,25 @@ $(function() {
 					opacity: '1'
 				}, 10);
 
-				$('.footer-wrapper').stop().animate({
-					opacity: '0'
-				}, 10);
-
 			} else if (currPos >= offset*2/3 && atTop) {
 
-				// $('#navbar').stop().animate({top: '0%'}, 800);
 				$('#navbar').addClass("active");
 				atTop = false;
 
 				$('#description').addClass("showing");
-				// $('#description').stop().animate({
-				// 	opacity: '1',
-				// 	margin: '17.5% 20%'
-				// }, 500);
 
 				// name on blurred image at top
 				$('#intro_title').stop().animate({
 					opacity: '0'
 				}, 10);
-
-				$('.footer-wrapper').stop().animate({
-					opacity: '1'
-				}, 10);
 			}
 
 			// nav bar coloring code
 			if (currPos > projectsTop-50) { // change nav to black, icons and text to white
-				// $('#navbar ul').stop().animate({
-				// 		backgroundColor: '#ffffff'
-				// 	}, 100);
 
 				$('#navbar ul').addClass("white");
-
 				$('#navbar ul li a').addClass("white");
-				// $('#navbar ul li a').stop().animate({
-				// 		color: '#000000'
-				// 	}, 100);
-
 				$('#nav_title a').addClass("black");
-				// $('#nav_title a').stop().animate({
-				// 		color: '#000000'
-				// 	}, 100);
 
 				$('#twitter').attr('src', 'img/twitter_black.png');
 				$('#linkedin').attr('src', 'img/linkedin_black.png');
@@ -97,43 +70,15 @@ $(function() {
 
 			} else { // change nav to white, icons and text to black
 			
-				// $('#navbar ul').stop().animate({
-				// 		backgroundColor: '#000000',
-				// 		color: '#ffffff'
-				// 	}, 100);
 				$('#navbar ul').removeClass("white");
-
-
 				$('#navbar ul li a').removeClass("white");
-
-				// $('#navbar ul li a').stop().animate({
-				// 		color: '#ffffff'
-				// 	}, 100);
-
 				$('#nav_title a').removeClass("black");
-
-				// $('#nav_title a').stop().animate({
-				// 		color: '#ffffff'
-				// 	}, 100);
 
 				$('#twitter').attr('src', 'img/twitter_white.png');
 				$('#linkedin').attr('src', 'img/linkedin_white.png');
 				$('#github').attr('src', 'img/octocat_white.png');
 
 			}
-
-			// scroll to bottom when 3/4 way through footer
-			if (pos > lastScroll && $(window).scrollTop() + $(window).height() >= $(document).height()-75) {
-				$('html, body').stop().animate({
-					scrollTop: $(document).height()
-				}, 250);
-			}
-
-			if($(window).scrollTop() + $(window).height() == $(document).height()) {
-		       $('.footer-wrapper').css('z-index', 1);
-		    } else {
-		    	$('.footer-wrapper').css('z-index', -2);
-		    }
 
 		    lastScroll = pos;
 
