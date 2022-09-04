4743bcccacc1483f6cb60f37fa15f6a78b5d406e
Clayton Rieck <cjrieck123@gmail.com>
2022-09-04 12:03:43 -0400

    add scroll restoration

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import {HashRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from "./components/scroll-to-top/scroll-to-top";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
